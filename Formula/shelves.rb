6a21160119d7a061827b3f98d3dd23ad0205ba17
Yunshu <yunshudai2010@gmail.com>
2023-09-09 15:46:18 +1000

    changed sha256

--- Formula/shelves.rb
+++ Formula/shelves.rb
@@ -2,7 +2,7 @@ class Shelves < Formula
   desc "Book tracker for your reading list and reading progress. - CLI"
   homepage "<https://github.com/pigeonseverywhere/shelves>"
   url "https://github.com/pigeonseverywhere/shelves/releases/download/v1.0.0/shelves.tar.gz"
-  sha256 "b3002c14ed07c25955d212f9cdd8c26ce3ac6537ffe6858df3d8233cd77e418f" 
+  sha256 "7c5453391ae9d28e88ca892676c1267b126f9d358f50e3c85b92b6766c480f59" 
   license "ISC"
   version "1.0.0"
 
