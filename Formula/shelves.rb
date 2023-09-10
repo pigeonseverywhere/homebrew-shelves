6184e08b14ab3eaf9d1b15595f79195a03a86143
Yunshu <yunshudai2010@gmail.com>
2023-09-10 20:28:49 +1000

    updated for release 1.0.10

--- Formula/shelves.rb
+++ Formula/shelves.rb
@@ -1,10 +1,10 @@
 class Shelves < Formula
   desc "Book tracker for your reading list and reading progress. - CLI"
   homepage "<https://github.com/pigeonseverywhere/shelves>"
-  url "<https://github.com/pigeonseverywhere/shelves/releases/download/v1.0.1/shelves.tar.gz>" 
-  sha256 "9b7778add3b80262c69a25402b073b518ad101a7439a1aadee42b174acb451b1" 
+  url "<https://github.com/pigeonseverywhere/shelves/releases/download/v1.0.10/shelves-cli.tar.gz>" 
+  sha256 "eec76353acd77cd07b380741605b1eb57e28735e025607c1b24bba654425993b" 
   license "ISC"
-  version "1.0.1"
+  version "1.0.10"
 
   def install
     bin.install "shelves"
