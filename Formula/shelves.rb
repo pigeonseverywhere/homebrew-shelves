f8d7330f9c1ce9d32741cd13e65b3a7d3fb50d57
Yunshu <yunshudai2010@gmail.com>
2023-09-10 20:32:33 +1000

    changed url format

--- Formula/shelves.rb
+++ Formula/shelves.rb
@@ -1,7 +1,7 @@
 class Shelves < Formula
   desc "Book tracker for your reading list and reading progress. - CLI"
   homepage "<https://github.com/pigeonseverywhere/shelves>"
-  url "<https://github.com/pigeonseverywhere/shelves/releases/download/v1.0.10/shelves-cli.tar.gz>" 
+  url "https://github.com/pigeonseverywhere/shelves/releases/download/v1.0.10/shelves-cli.tar.gz" 
   sha256 "eec76353acd77cd07b380741605b1eb57e28735e025607c1b24bba654425993b" 
   license "ISC"
   version "1.0.10"
